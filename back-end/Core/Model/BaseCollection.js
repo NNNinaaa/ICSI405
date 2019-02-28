22fefb5f022e3bd61f0769660eb96eb4e7e98eb8
Bat-orgil <daornit2@gmail.com>
2019-02-28 19:52:27 +0800

    Core оо зохиомжилч эхлэв.

---
+++ back-end/Core/Model/BaseCollection.js
@@ -0,0 +1,9 @@
+class BaseCollection{
+    constructor(){
+        this.createdDate = new Date();
+        this.updatedDate = new Date();
+        this.activeFlag = true;
+    }
+}
+
+export BaseCollection;
---
+++ back-end/ModelSchema/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const BaseCollection = require('./Core/Model/BaseCollection');
+
+class User extends BaseCollection{
+    constructor() {
+        super();
+    }
+
+}
